b9bec934d89e7a62a1bc079a66e00ab5a2e2861a
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-08-28 18:29:45 -0300

    refactor: add validators and email config/templates

---
+++ app/Validators/ResetPassword.js
@@ -0,0 +1,16 @@
+'use strict'
+
+class ResetPassword {
+  get validatorAll () {
+    return true
+  }
+
+  get rules () {
+    return {
+      token: 'required',
+      password: 'required|confirmed'
+    }
+  }
+}
+
+module.exports = ResetPassword
