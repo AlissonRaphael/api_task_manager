fc7dadf42d8533f72450a0757984e05449b5aa59
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-08-29 11:28:56 -0300

    feat: add sentry

---
+++ app/Exceptions/Handler.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const Config = use('Config')
+const Sentry = require('@sentry/node')
+const Env = use('Env')
+const Youch = use('youch')
+const BaseExceptionHandler = use('BaseExceptionHandler')
+
+/**
+ * This class handles all exceptions thrown during
+ * the HTTP request lifecycle.
+ *
+ * @class ExceptionHandler
+ */
+class ExceptionHandler extends BaseExceptionHandler {
+  /**
+   * Handle exception thrown during the HTTP lifecycle
+   *
+   * @method handle
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   * @param  {Object} options.response
+   *
+   * @return {void}
+   */
+  async handle (error, { request, response }) {
+    if (error.name === 'ValidationException') {
+      return response.status(error.status).send(error.messages)
+    }
+
+    if (Env.get('NODE_ENV') === 'development') {
+      const youch = new Youch(error, request.request)
+      const errorJSON = await youch.toJSON()
+
+      return response.status(error.status).send(errorJSON)
+    }
+
+    return response.status(error.status)
+  }
+
+  /**
+   * Report exception for logging or debugging.
+   *
+   * @method report
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   *
+   * @return {void}
+   */
+  async report (error, { request }) {
+    Sentry.init({
+      dsn: Config.get('service.sentry.dsn'),
+
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0
+    })
+
+    Sentry.captureException(error)
+  }
+}
+
+module.exports = ExceptionHandler
